019b53ac089d7ccad395927223ed06723ebd6afc
sascha <saschareza@gmail.com>
2023-03-30 17:46:16 +0200

    kill server

--- __tests__/server/server.test.ts
+++ __tests__/server/server.test.ts
@@ -2,9 +2,19 @@ import request from 'supertest';
 import app from '../../src/server/server';
 
 describe('Server', () => {
-    it('should return a 200 response for GET', async () => {
-        const response = await request(app).get('/');
-        expect(response.status).toBe(200);
-        expect(response.text).toBe('Hello, Personal Blog')
-    });
+  let server;
+
+  beforeAll(() => {
+    server = app.listen(3000);
+  });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+
+  it('should return a 200 response for GET', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello, Personal Blog')
+  });
 });
